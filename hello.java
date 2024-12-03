28c41cd3bd08009fc32bc12f878b209ca681cc05
Kavita Sharma <sharmakavitasharma25042005@gmail.com>
2024-12-03 12:53:13 +0530

    PrimeCheck program done

--- hello.java
+++ hello.java
@@ -1,7 +1,14 @@
 class Hello {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
       System.out.println("Hello World");
       System.out.println("Hello World Again ");
       System.out.println("Hello World firse");
+      int a=10;
+      System.out.println(a);
     }
   }
+    class Hello2{
+      public static void main(String args[]){
+      System.out.println("Hello2 World2");
+    }
+    }
